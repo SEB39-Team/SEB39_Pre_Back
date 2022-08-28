c2fddd0b4a74c4bfe94cf2d826622eaa65163657
lcjne00 <lcjne00@gmail.com>
2022-08-28 23:52:58 +0900

    Feat: "초기 세팅"

---
+++ src/main/java/com/codestates/pre047/Pre047Application.java
@@ -0,0 +1,13 @@
+package com.codestates.pre047;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pre047Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pre047Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/pre047/Pre047ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.pre047;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pre047ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
