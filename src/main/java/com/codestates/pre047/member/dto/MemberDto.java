ae4c06c4e471ea00ba4bf92b3cc0dbdea5ffee8c
lcjne00 <lcjne00@gmail.com>
2022-09-05 15:39:37 +0900

    Feature: MemberDto 구현

--- src/main/java/com/codestates/pre047/member/dto/MemberDto.java
+++ src/main/java/com/codestates/pre047/member/dto/MemberDto.java
@@ -3,31 +3,56 @@ package com.codestates.pre047.member.dto;
 import com.codestates.pre047.member.entity.Member;
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
 public class MemberDto {
-    private Long id;
-    private String email;
-    private String password;
-    private LocalDateTime createdDate;
-    private LocalDateTime modifiedDate;
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Post {
 
-    public Member toEntity(){
-        return Member.builder()
-                .id(id)
-                .email(email)
-                .password(password)
-                .build();
+        @NotBlank(message = "Name can't be empty")
+        @Size(message = "Name can't be more 15 characters", max = 15)
+        private String username;
+
+        @NotBlank(message = "Email can't be empty")
+        @Email(message = "Invalid email")
+        private String email;
+
+        @NotBlank(message = "Password can't be empty")
+        @Size(message = "Password can't be less 6 characters", min = 6)
+        private String password;
+
+        @NotBlank(message = "Password confirm can't be empty")
+        private String passwordConfirm;
+
     }
 
+    @Getter
+    @AllArgsConstructor
+    public static class Patch {
+
+        private long memberId;
+
+        @NotBlank(message = "Name can't be empty")
+        private String username;
+
+        @NotBlank
+        private String password;
+    }
+
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
     @Builder
-    public MemberDto(Long id, String email, String password) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
+    public static class Response {
     }
+
+    @NotBlank(message = "Name can't be empty")
+    private String username;
 }
