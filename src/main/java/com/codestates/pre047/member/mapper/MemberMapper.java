01bb853505c356a5a0fbfb7ec896c59022892cd3
lcjne00 <lcjne00@gmail.com>
2022-09-05 15:41:49 +0900

    Feature: Mapper 추가

---
+++ src/main/java/com/codestates/pre047/member/mapper/MemberMapper.java
@@ -0,0 +1,15 @@
+package com.codestates.pre047.member.mapper;
+
+
+import com.codestates.pre047.member.dto.MemberDto;
+import com.codestates.pre047.member.entity.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface MemberMapper {
+
+    Member memberPostDtoToMember(MemberDto.Post requestBody);
+
+    MemberDto.Response memberToMemberResponseDto(Member member);
+}
