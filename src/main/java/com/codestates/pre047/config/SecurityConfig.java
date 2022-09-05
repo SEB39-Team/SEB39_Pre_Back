5c65f1c2188b7028928983124d96f4440c2e2707
lcjne00 <lcjne00@gmail.com>
2022-09-05 23:08:16 +0900

    Feature: Jwt 회원가입 / 로그인 구현 2

--- src/main/java/com/codestates/pre047/config/SecurityConfig.java
+++ src/main/java/com/codestates/pre047/config/SecurityConfig.java
@@ -1,38 +1,69 @@
 package com.codestates.pre047.config;
 
+import com.codestates.pre047.config.filter.JwtAuthenticationFilter;
+import com.codestates.pre047.config.filter.JwtAuthorizationFilter;
 import com.codestates.pre047.member.repository.MemberRepository;
-import com.codestates.pre047.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-
-
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-/*
 
     private final MemberRepository memberRepository;
-
-
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder()  {
-        return new BCryptPasswordEncoder();
-    }
+    private final CorsFilter corsFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return null;
+
+
+        // http.addFilterBefore(new FirstFilter(), SecurityContextHolderFilter.class); // 주석 처리 or 제거
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .apply(new CustomDsl())
+                .and()
+                .authorizeRequests()
+                .antMatchers("/v1/members/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/v1/members/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/v1/members/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+
+        http.formLogin()
+                .loginPage("/v1/member/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/");
+
+
+
+        return http.build();
     }
-*/
 
+    public class CustomDsl extends AbstractHttpConfigurer<CustomDsl, HttpSecurity> {
 
-
-
-
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+            builder
+                    .addFilter(corsFilter)
+                    .addFilter(new JwtAuthenticationFilter(authenticationManager))
+                    .addFilter(new JwtAuthorizationFilter(authenticationManager,memberRepository));
+        }
+    }
 
 }
