5c2cef267ef7fd27dab190fa68f750f5041904eb
gschan01 <gschan011@gmail.com>
2022-09-05 11:37:10 +0900

    newlogin

--- src/main/java/com/codestates/pre047/answer/repository/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codestates.pre047.answer.repository;
-
-import com.codestates.pre047.answer.entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-}
--- src/main/java/com/codestates/pre047/post/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codestates.pre047.post.repository;
-
-import com.codestates.pre047.post.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
